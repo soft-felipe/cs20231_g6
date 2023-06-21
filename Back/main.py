d587343aa3b628c31bde4449eb21c1449e979303
nogueiralegacy <daniel.nogueira.dacosta@gmail.com>
2023-06-21 20:13:44 -0300

    Incluindo rotas

--- Back/main.py
+++ Back/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from routes import usuario_router, test_router
 app = FastAPI()
 
 @app.get("/")
 def home():
     return "Salve"
 
+app.include_router(usuario_router)
+app.include_router(test_router)
