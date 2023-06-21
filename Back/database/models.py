c8fcfa0a5298c82cd47292d6c2b9f5bcf5f7d01e
nogueiralegacy <daniel.nogueira.dacosta@gmail.com>
2023-06-21 19:25:46 -0300

    atualizando modelos para chave estrangeira

--- Back/database/models.py
+++ Back/database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Integer, Date
+from sqlalchemy import Column, String, Integer, Date, ForeignKey
 from database.base import Base
 from sqlalchemy_serializer import SerializerMixin
 
@@ -6,7 +6,7 @@ class UsuarioModel(Base, SerializerMixin):
     __tablename__ = 'usuario'
     
     id_usuario = Column('id_usuario', Integer, primary_key=True, autoincrement=True)
-    id_credencial = Column('id_credencial', Integer, foreign_key=True)
+    id_credencial = Column('id_credencial', Integer, ForeignKey('login.id_login'), nullable=True)
     apelido = Column('apelido', String, nullable=True)
     nome_completo = Column('nome_completo', String, nullable=True)
     data_nasc = Column('data_nasc', Date, nullable=True)
@@ -17,6 +17,6 @@ class UsuarioLoginModel(Base, SerializerMixin):
     __tablename__ = 'login'
     
     id_login = Column('id_login', Integer, primary_key=True, autoincrement=True)
-    login = Column('login', String, nullable=False)
+    username = Column('login', String, nullable=False)
     senha = Column('senha', String, nullable=False)
     email = Column('email', String, nullable=True)
