90038182fe4c714e0722d3ebcc5c7db8b4ecf8d6
nogueiralegacy <daniel.nogueira.dacosta@gmail.com>
2023-06-21 19:32:29 -0300

    organizando classes q representam as tabelas

--- Back/schemas.py
+++ Back/schemas.py
@@ -3,17 +3,14 @@ import datetime
 import re
 
 class Usuario(BaseModel):
-    id_usuario: int
-    id_credencial: int
     apelido: str
     nome_completo: str
-    data_nascimento: datetime.date
+    data_nasc: datetime.date
     avatar: str
     data_criacao: datetime.date
     
 class UsuarioLogin(BaseModel):
-    id_login: int
-    login: str
+    username: str
     senha: str
     email: EmailStr
     
