87f62b122fbad0a3a23d504c86f17751eb7be42d
soft-felipe <felipem791@gmail.com>
2023-07-21 20:55:59 -0300

    feat: mapeando schemas do banco

--- Back/model/schemas.py
+++ Back/model/schemas.py
@@ -10,6 +10,7 @@ class Usuario(BaseModel):
     data_nasc: datetime.date
     avatar: str
 
+
 class UsuarioLogin(BaseModel):
     username: str
     senha: str
@@ -21,23 +22,29 @@ class UsuarioLogin(BaseModel):
             raise ValueError('Formato invalido de nome de usuario')
         return value
 
-class Comentario(BaseModel):
-    id: int
-    descricao: str
-
-class Tarefa(BaseModel):
-    id: int
-    titulo: str
-    descricao: str
-    pontuacao: int
-    prioridade: int
-    comentarios: List[Comentario]
-   
-class Etapa(BaseModel):
-    id: int
-    titulo: str
-    tarefas: List[Tarefa]
 
 class Projeto(BaseModel):
     nome: str
     descricao: str
+
+
+class Comentario(BaseModel):
+    id: int
+    id_criador: int
+    id_tarefa: int
+    descricao: str
+
+
+class Tarefa(BaseModel):
+    id: int
+    id_criador: int
+    id_responsavel: int
+    id_etapa: int
+    descricao: str
+    comentarios: List[Comentario]
+
+
+class Etapa(BaseModel):
+    id: int
+    titulo: str
+    tarefas: List[Tarefa]
